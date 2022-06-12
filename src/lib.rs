b53df98236f843c2b9e3a57ab7cbd1fcd30e63db
Jonas <jonas@jole.xyz>
2022-06-12 15:44:53 +0200

    add firmware dump

--- src/lib.rs
+++ src/lib.rs
@@ -57,34 +57,45 @@ pub struct Hhkb {
 
 impl Hhkb {
     /// Send `msg` and return the response if `prefix` is a prefix of the response
-    fn send(&self, msg: &[u8], prefix: &[u8]) -> Option<[u8; 64]> {
-        let mut output = [0u8; 64];
-        output[..msg.len()].copy_from_slice(msg);
+    fn query(&self, msg: &[u8], prefix: &[u8]) -> Option<[u8; 64]> {
+        self.send(msg);
 
-        let res = self.dev.write(&output).unwrap();
-        assert_eq!(res, 65);
+        let buf = self.receive();
 
-        let mut buf = [0u8; 64];
-        let res = self.dev.read(&mut buf[..]).unwrap();
-        assert_eq!(res, 64);
-
-        let veri = buf
-            .iter()
-            .zip(prefix.iter())
-            .map(|(a, b)| a == b)
-            .reduce(|last, new| if !last { false } else { new })
-            .unwrap();
-
-        if veri {
+        if Hhkb::verify(&buf, &prefix) {
             Some(buf)
         } else {
             None
         }
     }
 
+    fn verify(a: &[u8], b: &[u8]) -> bool {
+        a.iter()
+            .zip(b.iter())
+            .map(|(a, b)| a == b)
+            .reduce(|last, new| if !last { false } else { new })
+            .unwrap()
+    }
+
+    fn send(&self, msg: &[u8]) {
+        let mut output = [0u8; 64];
+        output[..msg.len()].copy_from_slice(msg);
+
+        let res = self.dev.write(&output).unwrap();
+        assert_eq!(res, 65);
+    }
+
+    fn receive(&self) -> [u8; 64] {
+        let mut buf = [0u8; 64];
+        let res = self.dev.read(&mut buf[..]).unwrap();
+        assert_eq!(res, 64);
+
+        buf
+    }
+
     pub fn dips(&self) -> [bool; 6] {
         let buf = self
-            .send(&[0x00, 0xAA, 0xAA, 0x05], &[85, 85, 5, 0, 0, 12])
+            .query(&[0x00, 0xAA, 0xAA, 0x05], &[85, 85, 5, 0, 0, 12])
             .unwrap();
 
         let mut out = [false; 6];
@@ -95,7 +106,7 @@ impl Hhkb {
 
     pub fn mode(&self) -> KeyboardMode {
         let buf = self
-            .send(&[0x00, 0xAA, 0xAA, 0x06], &[85, 85, 6, 0, 0, 1])
+            .query(&[0x00, 0xAA, 0xAA, 0x06], &[85, 85, 6, 0, 0, 1])
             .unwrap();
 
         buf[6].try_into().unwrap()
@@ -103,7 +114,7 @@ impl Hhkb {
 
     pub fn info(&self) -> KeyboardInfo {
         let buf = self
-            .send(&[0x00, 0xAA, 0xAA, 0x02], &[85, 85, 2, 0, 0 /*, 48*/])
+            .query(&[0x00, 0xAA, 0xAA, 0x02], &[85, 85, 2, 0, 0, 57])
             .unwrap();
 
         fn s_enc(vec: Vec<u8>) -> String {
@@ -132,6 +143,38 @@ impl Hhkb {
             boot_firm_version,
             running_firmware,
         }
+    }
+
+    pub fn dump(&self) -> Option<Vec<u8>> {
+        self.send(&[0x00, 0xAA, 0xAA, 0xD0, 0x00, 0x00]);
+
+        let mut result = vec![];
+
+        let mut buffer;
+        loop {
+            buffer = self.receive();
+            if !Hhkb::verify(&buffer, &[85, 85, 208, 0, 0]) {
+                return None;
+            }
+
+            let (header, body) = buffer.split_at(8);
+
+            let len = header[5] - 2;
+            let idx = u16::from_be_bytes(header[6..8].try_into().unwrap());
+
+            let data = &body[..len as usize];
+            println!("{}: read {} bytes", idx, len);
+
+            result.extend_from_slice(&data);
+
+            // what if the last piece is 58 long? will it send a new with 0?
+            // this is how PFU does it so i guess we'll also do it
+            if len < 56 {
+                break;
+            }
+        }
+
+        Some(result)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::Write};
+
 fn main() {
     let kb = hhkbfit::get_dev();
 
@@ -6,4 +8,15 @@ fn main() {
     println!("mode: {:?}", kb.mode());
 
     println!("info: {:#?}", kb.info());
+
+    let firmware = kb.dump().unwrap();
+    File::create("firmware.bin")
+        .unwrap()
+        .write_all(&firmware)
+        .unwrap();
+    File::create("firmware.hex")
+        .unwrap()
+        .write_all(&hex::encode(&firmware).into_bytes())
+        .unwrap();
+    println!("firmware written to firmware.{{bin,hex}}");
 }
